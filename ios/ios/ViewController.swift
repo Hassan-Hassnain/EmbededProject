e9f798134bd5db19fba91a37444fb1a21e5dbf61
Hassan-Hassnain <ahh.devoders@gmail.com>
2020-11-25 20:33:42 +0500

    React Native integrated and working as per tutorial

---
+++ ios/ios/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  ios
+//
+//  Created by اسرارالحق  on 25/11/2020.
+//
+
+import UIKit
+import React
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        highScoreButtonTapped()
+    }
+
+     func highScoreButtonTapped() {
+        NSLog("Hello")
+        let jsCodeLocation = URL(string: "http://localhost:8081/index.bundle?platform=ios")
+        let mockData:NSDictionary = ["scores":
+                                        [
+                                            ["name":"Alex", "value":"42"],
+                                            ["name":"Joel", "value":"10"]
+                                        ]
+        ]
+        
+        let rootView = RCTRootView(
+            bundleURL: jsCodeLocation!,
+            moduleName: "RNHighScores",
+            initialProperties: mockData as [NSObject : AnyObject],
+            launchOptions: nil
+        )
+        let vc = UIViewController()
+        vc.view = rootView
+        self.present(vc, animated: true, completion: nil)
+    }
+
+}
+
